ce00b7de8eed5a5d147343e8e18845899f0dbba6
Dimercel <xolcman@gmail.com>
2025-08-25 20:59:12 +0300

    Stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod color;
+mod vec3;
+
+use std::io;
+
+use color::Color;
+
 fn main() {
     // Image
  
@@ -14,6 +21,8 @@ fn main() {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
             let b = 0.25;
+            let pixel_color = Color::new(r, g, b);
+            color::write_color(&mut io::stdout(), pixel_color);
  
             let ir = (255.999 * r) as i32;
             let ig = (255.999 * g) as i32;
